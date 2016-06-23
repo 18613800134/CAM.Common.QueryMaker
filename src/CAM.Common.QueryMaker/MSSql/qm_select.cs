6ead7f3d455e8abebe7637c420695d6c4e4a23bf
iambluebird <6891122@qq.com>
2016-06-23 09:40:51 +0800

    修复select语句中出现null值导致映射出错的BUG

--- src/CAM.Common.QueryMaker/MSSql/qm_select.cs
+++ src/CAM.Common.QueryMaker/MSSql/qm_select.cs
@@ -22,7 +22,11 @@ namespace CAM.Common.QueryMaker.MSSql
                 string viewModeName = viewMode.ViewMode.Name;
                 foreach (PropertyInfo pi in viewMode.ViewMode.GetProperties())
                 {
-                    viewItems.Add(string.Format("[{0}].[{1}] as [{2}_{1}] ", viewMode.Alias, pi.Name, viewModeName));
+                    
+                    //Console.WriteLine("pi.name={0}, pi.type={1}", pi.Name, pi.PropertyType.Name);
+                    //viewItems.Add(string.Format("[{0}].[{1}] as [{2}_{1}] ", viewMode.Alias, pi.Name, viewModeName));
+                    string itemName = formatNullItemDefaultValue(string.Format("[{0}].[{1}]", viewMode.Alias, pi.Name), pi.PropertyType);
+                    viewItems.Add(string.Format("{0} as [{2}_{1}] ", itemName, pi.Name, viewModeName));
                 }
             }
             sbStr.AppendFormat("{0} ", string.Join(",", viewItems));
@@ -30,6 +34,28 @@ namespace CAM.Common.QueryMaker.MSSql
             sbStr.AppendFormat("from [{0}] as [{1}] ", obj.Entity.Name, obj.Alias);
 
             return sbStr.ToString();
+        }
+
+        private string formatNullItemDefaultValue(string piName, Type piType)
+        {
+            string result = "";
+            switch (piType.Name.Trim().ToLower())
+            {
+                case "int64":
+                case "int32":
+                    result = string.Format(" isnull({0}, 0) ", piName);
+                    break;
+                case "boolean":
+                    result = string.Format(" isnull({0}, 1) ", piName);
+                    break;
+                case "string":
+                    result = string.Format(" isnull({0}, '') ", piName);
+                    break;
+                case "datetime":
+                    result = string.Format(" isnull({0}, getdate()) ", piName);
+                    break;
+            }
+            return result;
         }
     }
 }
